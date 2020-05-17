aff1c7b2c80282fe5c6c62915db39f96fd00e32d
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-05-17 12:51:42 +0200

    connect controller and view

--- controller/controller.php
+++ controller/controller.php
@@ -8,4 +8,5 @@ function listPosts()
     $postManager = new PostManager(); // Création d'un objet
     $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
 
+    require('view/listPostsView.php');
 }
