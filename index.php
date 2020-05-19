004104b621037dd1e1ab27545fc8cd2e1d5ecde2
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-05-19 10:38:14 +0200

    problem solving

--- index.php
+++ index.php
@@ -6,14 +6,15 @@ try{
         if ($_GET['action'] == 'listPosts') {
             listPosts();
         }
-    }
-    elseif ($_GET['action'] == 'post') {
-        if (isset($_GET['id']) && $_GET['id'] > 0) {
-            post();
-        }
-        else {
-            // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
-            throw new Exception('Aucun identifiant de billet envoyé');
+    
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post($_GET['id']);
+            }
+            else {
+                // Erreur ! On arrête tout, on envoie une exception, donc au saute directement au catch
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
         }
     }
     else {
