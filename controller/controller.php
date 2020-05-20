cc16ea9b980b4f7c2a75aa2f8ebe10d15442f3ad
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-05-20 16:55:46 +0200

    add function addComment in controller

--- controller/controller.php
+++ controller/controller.php
@@ -22,3 +22,17 @@ function post()
 
     require('view/postView.php');
 }
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
+}
