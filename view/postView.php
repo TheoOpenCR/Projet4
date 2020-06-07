2ca20d0f967d0f46e5247de828cfb627ad378a27
Théo Saint-jalmes <theosaint-jalmes@air-de-theo.home>
2020-06-07 11:43:18 +0200

    code improvement

--- view/postView.php
+++ view/postView.php
@@ -1,50 +1,45 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Billet simple pour l'Alaska</title>
-        <link href="public/css/style.css" rel="stylesheet" /> 
-    </head>
+<?php $title = htmlspecialchars($post['title']); ?>
+ 
+ <?php ob_start(); ?>
+    <h1>Roman de Jean Forteroche</h1>
+    <p><a href="index.php">Retour à la liste des chapitres</a></p>
+
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']) ?>
+            <em>le <?= $post['creation_date_fr'] ?></em>
+        </h3>
         
-    <body>
-        <h1>Roman de Jean Forteroche</h1>
-        <p><a href="index.php">Retour à la liste des chapitres</a></p>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content'])) ?>
+        </p>
+    </div>
 
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']) ?>
-                <em>le <?= $post['creation_date_fr'] ?></em>
-            </h3>
-            
-            <p>
-                <?= nl2br(htmlspecialchars($post['content'])) ?>
-            </p>
+    <h2>Commentaires</h2>
+
+    <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+        <div>
+            <label for="author">Nom : </label><br />
+            <input type="text" id="author" name="author" />
         </div>
+        <div>
+            <label for="comment">Commentaire : </label><br />
+            <textarea id="comment" name="comment"></textarea>
+        </div>
+        <div>
+            <input type="submit" />
+        </div>
+    </form>
 
-        <h2>Commentaires</h2>
+    <?php
+    while ($comment = $comments->fetch())
+    {
+    ?>
+        <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
+        <p><?= nl2br(htmlspecialchars($comment['comment'])) ?><a href="index.php?action=report">Signaler</a></p>
+    <?php
+    }
+    ?>
+<?php $content = ob_get_clean(); ?>
 
-        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-            <div>
-                <label for="author">Nom : </label><br />
-                <input type="text" id="author" name="author" />
-            </div>
-            <div>
-                <label for="comment">Commentaire : </label><br />
-                <textarea id="comment" name="comment"></textarea>
-            </div>
-            <div>
-                <input type="submit" />
-            </div>
-        </form>
-
-        <?php
-        while ($comment = $comments->fetch())
-        {
-        ?>
-            <p><strong><?= htmlspecialchars($comment['author']) ?></strong> le <?= $comment['comment_date_fr'] ?></p>
-            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-        <?php
-        }
-        ?>
-    </body>
-</html>
+<?php require(__DIR__.'/template.php'); ?>
