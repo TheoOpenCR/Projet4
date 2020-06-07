c7683ce5615821e427b5c7f208f5426954e2b57d
Théo Saint-jalmes <theosaint-jalmes@air-de-theo.home>
2020-06-07 11:44:07 +0200

    add function login in controller

--- controller/controller.php
+++ controller/controller.php
@@ -1,15 +1,16 @@
 <?php
 
 // Chargement des classes
-require_once('model/PostManager.php');
-require_once('model/CommentManager.php');
+require_once(__DIR__."/../model/PostManager.php");
+require_once(__DIR__."/../model/CommentManager.php");
+require_once(__DIR__."/../model/AdminManager.php");
 
 function listPosts()
 {
     $postManager = new PostManager(); // Création d'un objet
     $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
 
-    require('view/listPostsView.php');
+    require(__DIR__."/../view/listPostsView.php");
 }
 
 function post()
@@ -20,7 +21,7 @@ function post()
     $post = $postManager->getPost($_GET['id']);
     $comments = $commentManager->getComments($_GET['id']);
 
-    require('view/postView.php');
+    require(__DIR__."/../view/postView.php");
 }
 
 function addComment($postId, $author, $comment)
@@ -35,4 +36,42 @@ function addComment($postId, $author, $comment)
     else {
         header('Location: index.php?action=post&id=' . $postId);
     }
+}
+
+function login()
+{
+    try{
+        if (isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+            $username = htmlspecialchars($_POST['username']);
+            $password = htmlspecialchars($_POST['password']);
+    
+            $adminManager = new AdminManager();
+            $user = $adminManager->getLogin($username);
+
+            if (isset($user['username']) && $_POST['username'] == $user['username'] && $_POST['password'] == $user['password']) {
+                $_SESSION['admin'] = true;
+                $_SESSION['username'] = $user;
+                header('location: /../view/adminView.php');
+            }
+            else {
+                throw new Exception('Mauvais identifiant ou mot de passe !');
+            }
+        }
+        else {
+            throw new Exception('Tous les champs ne sont pas remplis !');
+        }
+    
+        require(__DIR__."/../view/adminView.php");
+        require(__DIR__."/../view/login.php");
+    }
+    catch(Exception $e) { // S'il y a eu une erreur, alors...
+?>
+    <div id="error">
+        <?php
+        echo 'Erreur : ' . $e->getMessage();
+        ?>
+    </div>
+<?php
+    }
+
 }
