90a395d78e66a8ee56328482ceac43dea481c267
Théo Saint-jalmes <theosaint-jalmes@air-de-theo.home>
2020-07-10 11:50:44 +0200

    add functions to login and about the comment

--- controller/controller.php
+++ controller/controller.php
@@ -1,15 +1,18 @@
+<php session_start() ?>
 <?php
 
+
 // Chargement des classes
-require_once('model/PostManager.php');
-require_once('model/CommentManager.php');
+require_once(__DIR__."/../model/PostManager.php");
+require_once(__DIR__."/../model/CommentManager.php");
+require_once(__DIR__."/../model/AdminManager.php");
 
 function listPosts()
 {
     $postManager = new PostManager(); // Création d'un objet
     $posts = $postManager->getPosts(); // Appel d'une fonction de cet objet
 
-    require('view/listPostsView.php');
+    require(__DIR__."/../view/listPostsView.php");
 }
 
 function post()
@@ -20,7 +23,7 @@ function post()
     $post = $postManager->getPost($_GET['id']);
     $comments = $commentManager->getComments($_GET['id']);
 
-    require('view/postView.php');
+    require(__DIR__."/../view/postView.php");
 }
 
 function addComment($postId, $author, $comment)
@@ -36,3 +39,92 @@ function addComment($postId, $author, $comment)
         header('Location: index.php?action=post&id=' . $postId);
     }
 }
+
+function login()
+{
+    try{
+        if (isset($_POST['username']) && isset($_POST['password']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+            $username = htmlspecialchars($_POST['username']);
+            $password = htmlspecialchars($_POST['password']);
+    
+            $adminManager = new AdminManager();
+            $user = $adminManager->getLogin($username);
+
+            if (isset($user['username']) && $_POST['username'] == $user['username'] && $_POST['password'] == $user['password']) {
+                $_SESSION['admin'] = true;
+                $_SESSION['username'] = $user['username'];
+                showReportComment();
+                
+                
+            }
+            else {
+                throw new Exception( 'Mauvais identifiant ou mot de passe ! ');
+            }
+        }
+        else {
+            throw new Exception('Tous les champs ne sont pas remplis ! ');
+        }
+    }
+    catch(Exception $e) { // S'il y a eu une erreur, alors...
+?>
+    <div id="error">
+        <?php
+            header('Location: ../view/login.php' ); 
+            echo 'Erreur : ' . $e->getMessage();
+        ?>
+    </div>
+<?php
+    }
+
+}
+
+function reportComment($id)
+{
+    $commentManager = new CommentManager();
+    $comment = $commentManager->reportComments($id);
+
+    echo "Votre commentaire à bien été signalé, il sera traité au plus vite par les administateurs.";
+    ?>
+    <p><a class="buttonBack" href="../index.php">Retour à la liste des chapitres</a></p>
+    <?php
+
+}
+
+function showReportComment()
+{
+    $commentManager = new CommentManager();
+
+    $reportedComment = $commentManager->reportedComments();
+
+    require(__DIR__."/../view/adminView.php");
+
+}
+
+function cancelReporting($id) 
+{
+    $commentManager = new CommentManager();
+
+    $cancelReporting = $commentManager->cancelcommentReporting($id);
+
+    header('Location: ../view/adminView.php' ); 
+
+    echo "Le commentaire n'est plus signalé. Vous avez jugé qu'il étais approprié . ";
+    ?>
+    <p><a class="buttonBack" href="../index.php">Retour à la liste des chapitres</a></p>
+    <?php
+}
+
+function deleteCommentReporting($id)
+{
+    $commentManager = new CommentManager();
+
+    $deleteComment = $commentManager->deleteComment($id);
+
+    require(__DIR__."/../view/adminView.php");
+
+    echo "Le commentaire à été supprimé";
+    ?>
+    <p><a class="buttonBack" href="../index.php">Retour à la liste des chapitres</a></p>
+    <?php
+}
+?>
