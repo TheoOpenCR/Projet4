9a074d85f28006769ffc7518fe0199a9cf3558ab
Théo Saint-jalmes <theosaint-jalmes@air-de-theo.home>
2020-07-10 11:58:20 +0200

    add function to delete and cancel a comment report

--- model/CommentManager.php
+++ model/CommentManager.php
@@ -6,7 +6,7 @@ class CommentManager extends Manager
     public function getComments($postId)
     {
         $db = $this->dbConnect();
-        $comments = $db->prepare('SELECT id, author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
+        $comments = $db->prepare('SELECT id, author, comment, is_reporting, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE post_id = ? ORDER BY comment_date DESC');
         $comments->execute(array($postId));
 
         return $comments;
@@ -34,8 +34,26 @@ class CommentManager extends Manager
     public function reportedComments() 
     {
         $db = $this->dbConnect();
-        $reportedComment = $db->query('SELECT comment, is_reporting FROM comments');
+        $reportedComment = $db->query('SELECT comment, is_reporting, id FROM comments WHERE is_reporting = 1');
 
         return $reportedComment;
+    }
+
+    public function cancelcommentReporting($id)
+    {
+        $db = $this->dbConnect();
+        $cancelReporting = $db->prepare('UPDATE comments SET is_reporting = 0 WHERE id = ?');
+        $cancelReporting->execute(array($id));
+
+        return $cancelReporting;
+    }
+
+    public function deleteComment($id)
+    {
+        $db = $this->dbConnect();
+        $deleteComment = $db->prepare('DELETE FROM comments WHERE id = ?');
+        $deleteComment->execute(array($id));
+
+        return $deleteComment;
     }
 }
