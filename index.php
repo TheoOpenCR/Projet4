41f1c6ceceba3770f61191f7c1e232281ac296e0
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-07-20 12:28:47 +0200

    add different action in the router to call the good controller

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
+<?php session_start(); ?>
 <?php
 require('controller/controller.php');
-
 try{
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'listPosts') {
@@ -31,13 +31,67 @@ try{
                 // Autre exception
                 throw new Exception('Aucun identifiant de billet envoyé');
             }
+        }   
+
+        elseif ($_GET['action'] == "login") {
+            header('location: view/login.php');
         }
+
+        elseif ($_GET['action'] == 'report') {
+
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                reportComment($_GET['id']);
+            }
+
+            else {
+                // Autre exception
+                throw new Exception('Aucun identifiant de commentaire envoyé');
+            }
+            
+        }
+
+        elseif (isset($_GET['action']) && $_GET['action'] == "admin") {
+            login();
+        }
+
+        elseif ($_GET['action'] == "cancelReport") {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                cancelReporting($_GET['id']);
+            }
+
+            else {
+                // Autre exception
+                throw new Exception('Aucun identifiant de commentaire envoyé');
+            }
+        }
+
+        elseif ($_GET['action'] == "deleteComment") {
+            deleteCommentReporting($_GET['id']);
+        }
+
+        elseif ($_GET['action'] == 'addPost') {
+                if (!empty($_POST['title']) && !empty($_POST['chapter_content']) && !empty($_POST['picture'])) {
+                    addPost($_POST['title'], $_POST['chapter_content'], $_POST['picture']);
+                }
+                else {
+                    // Autre exception
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+        }   
+
+
     }
     else {
         listPosts();
     }
-        
+
+    
 }
 catch(Exception $e) { // S'il y a eu une erreur, alors...
-    echo 'Erreur : ' . $e->getMessage();
+    ?>
+    <?php
+        $errorMessage = $e->getMessage();
+        require(__DIR__.'/View/errorView.php');
+    ?>
+    <?php
 }
