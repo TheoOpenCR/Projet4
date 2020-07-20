40ce4a20e2b71538d9a37de4df56183b3944eadb
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-07-20 12:27:21 +0200

    add function postPost in the model

--- model/PostManager.php
+++ model/PostManager.php
@@ -1,13 +1,13 @@
 <?php
-require_once("model/Manager.php");
+require_once(__DIR__."/Manager.php");
 
 class PostManager extends Manager
 {
     public function getPosts()
     {
         $db = $this->dbConnect();
-        $req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
-
+        $req = $db->query('SELECT id, title, picture, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5');
+        
         return $req;
     }
 
@@ -19,5 +19,17 @@ class PostManager extends Manager
         $post = $req->fetch();
 
         return $post;
+    }
+
+    public function postPost($title, $content, $picture)
+    {
+        $db = $this->dbConnect();
+        $posts = $db->prepare('INSERT INTO posts(title, content, picture, creation_date) VALUES(?, ?, ?, NOW())');
+        $affectedLines = $posts->execute(array($title, $content, $picture));
+
+        print_r($posts->errorInfo());
+
+
+        return $affectedLines;
     }
 }
