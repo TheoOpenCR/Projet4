1bf7258f695c3ebcf68c1718a36f488e4366e6ab
Théo Saint-jalmes <theosaint-jalmes@MacBook-Air-de-Theo.local>
2020-07-20 12:30:50 +0200

    small changes in the view to login

--- view/login.php
+++ view/login.php
@@ -1,22 +1,19 @@
-<?php require(__DIR__."/../controller/controller.php");?>
-<?php
-if (isset($_POST["OK"])) {
-    login();
-}
-?>
-
 <?php $title = 'Administration'; ?>
 
 <?php ob_start(); ?>
+<p><a class="buttonBack" href="../index.php">Retour à la liste des chapitres</a></p>
+
+<div class="img">
+        <img src="https://res.cloudinary.com/theosj/image/upload/w_432/v1593692578/Projet4/panorama_zhixog.jpg">
+</div>
 <div class = "form">
-    <form action="" method="post">
-        <p>Se connecter à l'espace d'administration : </p>
-        Pseudo : <input type="text" name="username" placeholder = "username">
-        Mot de passe : <input type="password" name="password" placeholder = "Mot de passe">
+    <form action="../index.php?action=admin" method="post">
+        <p>LOGIN </p>
+        <input type="text" name="username" placeholder = "Username" class="inputLogin"> <br />
+        <input type="password" name="password" placeholder = "Mot de passe" class="inputLogin"> <br />
         <input type="submit" name="OK" id="OK" value="Connecte toi !">
     </form>
 </div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require(__DIR__.'/template.php'); ?>
-
